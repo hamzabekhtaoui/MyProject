982b9ebca34ac6f4a495aa1de96db593f878b716
Hamza BEKHTAOUI <hamzabekhtaoui1994@gmail.com>
2019-06-12 17:39:49 +0200

    Modification of file create vpc and ec2

--- s3Bucket.tf
+++ s3Bucket.tf
@@ -1,14 +1,54 @@
 provider "aws" {
   region  = "eu-west-1"
-  profile = "PowerUserAccess"
+  profile = "DeepRacer"
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bfffucket"
+  bucket = "my-tf-test-bfffucket-one"
   acl    = "private"
 
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
+  }
+}
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = "${aws_vpc.my_vpc.id}" 
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = "${aws_subnet.my_subnet.id}"
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-030dbca661d402413"
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = "${aws_network_interface.foo.id}"
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
   }
 }
